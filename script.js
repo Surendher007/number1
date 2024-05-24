c124ecbd29c58514bd81fde0cb417d676fbaffd0
Surendher007 <surendherbabu13@gmail.com>
2024-05-24 06:49:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let array = [1,2,3,4,5,6,7,8,9,10]
+let display = array.filter(n => n%2 ==0)
+console.log(display)
+
+// (function(){
+//     let array = [1,2,3,4,5,6,7,8,9,10]
+//     let display = array.filter(n => n%2 ==0)
+//     console.log(display)
+// })()
