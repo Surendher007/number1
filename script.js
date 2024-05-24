5a883cbaee1b2f7cd0f2b0e6ccb28cfc813da453
Surendher007 <surendherbabu13@gmail.com>
2024-05-24 08:00:27 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+//a. Print odd numbers in an array
 
-let array = [1,2,3,4,5,6,7,8,9,10]
-let display = array.filter(n => n%2 ==0)
-console.log(display)
+// anonymous function
 
-// (function(){
-//     let array = [1,2,3,4,5,6,7,8,9,10]
-//     let display = array.filter(n => n%2 ==0)
-//     console.log(display)
-// })()
+let oddnum = function(arr){
+    let out = arr.filter(function(num){
+        return num%2 !==0
+    })
+    console.log(out)
+}
+oddnum([1,2,3,4,5,6,7,8,9,10]);
+
+// IIFE function
+
+(function(arr){
+    let out = arr.filter(function(num){
+        return num%2 !==0
+    })
+    console.log(out)
+})([1,2,3,4,5,6,7,8,9,10])
+
+// b. Convert all the strings to title caps in a string array
+
+// anonymous function
+
+
